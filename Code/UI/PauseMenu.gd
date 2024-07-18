0df23a8768491cc31d2dabf412404680f1e34b56
Ryuudin <156611256+Ryuudin@users.noreply.github.com>
2024-07-18 02:48:03 -0700

    Attempted to fix pause menu. Failed.

--- Code/UI/PauseMenu.gd
+++ Code/UI/PauseMenu.gd
@@ -7,15 +7,16 @@ extends Control
 func _ready():
 	self.visible = false
 	
-	
-func _input(event):
-	if event.is_action_pressed("exit"):
-		leavegame()
-	if event.is_action_pressed("resume"):
-		resume()
+#attempted to set focus on UI element to prevent conflictions, didn't work.
+func _process(_delta: float):
+	if get_tree().paused:
+		self.visible = true
+		grab_focus()
+	else:
+		self.visible = false
+		
 
-
-
+#These buttons do nothing?? WHY?!?!?
 func _on_resume_pressed():
 	get_tree().paused = false
 	hide()
@@ -26,6 +27,18 @@ func _on_quit_pressed():
 	get_tree().quit()
 	print("Quit button pressed")
 	
+	
+	
+	
+	
+	
+	#Attempt to fix another way failed. "F" button works when menu is not open, but once open no longer quits game.
+func _input(event):
+	if event.is_action_pressed("exit"):
+		leavegame()
+	if event.is_action_pressed("resume"):
+		resume()
+
 func leavegame():
 	get_tree().quit()
 	
@@ -33,3 +46,4 @@ func resume():
 	get_tree().paused = false
 	self.hide()
 	print("Resume button pressed")
+
